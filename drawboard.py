3fb153204d0ed1f3fd1de307baf53bb2fa9eb714
Maidul <u1703016@student.cuet.ac.bd>
2023-08-07 21:53:44 +0600

    [feat] show instructions, show control panel, monitor for keyboard input

--- drawboard.py
+++ drawboard.py
@@ -5,8 +5,10 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-from status import show_status
+from instructions import show_instructions
+from options import show_control_panel
 
 
 # -----------------------------------------------------------------------------------------------
@@ -17,14 +19,18 @@ from status import show_status
 # callback should go from the main window and various windows should be displayed under certain conditions
 
 is_left_down = False
+draw_mode = False
 
 
 def mouse_callback(event, x, y, flags, userdata):
     global is_left_down
 
     if event == cv.EVENT_LBUTTONDBLCLK:
-        show_status(can_draw=True)
+        pass
 
+
+# showing the instructions window
+instruction_window = show_instructions()
 
 # creating named window as the gui black-board, binding the window for observing mouse events,
 # and setting up mouse callback
@@ -36,10 +42,17 @@ cv.namedWindow("DrawBoard-GUI")
 cv.setMouseCallback("DrawBoard-GUI", mouse_callback)
 
 while True:
+    show_control_panel()
     cv.imshow("DrawBoard-GUI", drawboard_gui)
 
     keyboard_input = cv.waitKey(100)
     if keyboard_input == (ord("q") or 27):
         break
+    if keyboard_input == ord("c"):
+        cv.destroyWindow(instruction_window)
+    if keyboard_input == ord("d"):
+        draw_mode = True
+    if keyboard_input == ord("r"):
+        draw_mode == False
 
 cv.destroyAllWindows()
