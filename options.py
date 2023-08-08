fd38119c4ab4e6d18d319f281246799def1e445c
Maidul <u1703016@student.cuet.ac.bd>
2023-08-08 15:41:09 +0600

    [feat] Show drawing tool status in the main GUI; Update default paint brush color to (160, 160, 160)

--- options.py
+++ options.py
@@ -11,24 +11,26 @@ import matplotlib.pyplot as plt
 ###   Controller components of the program (color mixer, brush size, drawing pen status)    ###
 # -----------------------------------------------------------------------------------------------
 
-#-------------------------------------
+# -------------------------------------
 ### create the Control Panel window
-#-------------------------------------
+# -------------------------------------
 
 cv.namedWindow("Control_Panel")
 
-#-----------------
+# -----------------
 ### Color Mixer
-#-----------------
+# -----------------
 
 
 color_sample = np.ones((100, 400, 3), dtype=np.uint8)
+
 
 def get_color_component_values_from_tackbars():
     r = cv.getTrackbarPos("R", "Control_Panel")
     g = cv.getTrackbarPos("G", "Control_Panel")
     b = cv.getTrackbarPos("B", "Control_Panel")
     return (b, g, r)
+
 
 def color_mixer():
     b, g, r = get_color_component_values_from_tackbars()
@@ -42,9 +44,9 @@ def rgb_callback(pos, userdata=0):
     return color_mixer()
 
 
-cv.createTrackbar("R", "Control_Panel", 255, 255, rgb_callback)
-cv.createTrackbar("G", "Control_Panel", 255, 255, rgb_callback)
-cv.createTrackbar("B", "Control_Panel", 255, 255, rgb_callback)
+cv.createTrackbar("R", "Control_Panel", 160, 255, rgb_callback)
+cv.createTrackbar("G", "Control_Panel", 160, 255, rgb_callback)
+cv.createTrackbar("B", "Control_Panel", 160, 255, rgb_callback)
 
 
 def show_control_panel():
@@ -52,18 +54,22 @@ def show_control_panel():
     # function when called within a 'while True' loop in the main DrawBoard GUI file
     cv.imshow("Control_Panel", color_sample)
 
+
 # -----------------------------
 ### brush size and brush color
 # -----------------------------
 
-def nothing():
+
+def nothing(pos):
     pass
 
 
 cv.createTrackbar("Paint Brush Size: ", "Control_Panel", 1, 5, nothing)
 
+
 def paint_brush_size():
     return cv.getTrackbarPos("Paint Brush Size: ", "Control_Panel")
+
 
 def color_to_draw_with():
     return get_color_component_values_from_tackbars()
@@ -72,4 +78,18 @@ def color_to_draw_with():
 # ----------------------------------------------------------------
 ### Show status (whether drawing tool is engaged or released)
 # ----------------------------------------------------------------
+def update_drawing_tool_status(pos):
+    return cv.setTrackbarPos(
+        "Drawing Tool Dis-engaged/Released (0)\nDrawing Tool Engaged, Draw Mode (1): ",
+        "Control_Panel",
+        pos,
+    )
 
+
+cv.createTrackbar(
+    "Drawing Tool Dis-engaged/Released (0)\nDrawing Tool Engaged, Draw Mode (1): ",
+    "Control_Panel",
+    0,
+    1,
+    update_drawing_tool_status,
+)
